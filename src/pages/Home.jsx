2f046cdabf97a17c643a20ae621133de272cd445
lucasduarte0 <lucasduarte9120@gmail.com>
2023-01-20 00:50:12 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,15 @@ import Footer from "../sections/Footer";
 import Profile from "../sections/Profile";
 
 function Home() {
-    return (
-        <div className="Home">
-            <Profile />
-            {/* <Catalogue /> */}
-            <AxiosRequest />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="Home">
+      <main className="py-10">
+        <Profile />
+        <AxiosRequest />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
