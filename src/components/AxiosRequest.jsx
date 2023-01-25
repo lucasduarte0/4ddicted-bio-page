98aa7ae7c9007c7393e84f9a796b171741540792
Lucas <lucasduarte9120@gmail.com>
2023-01-25 17:13:03 -0300

    image change

--- src/components/AxiosRequest.jsx
+++ src/components/AxiosRequest.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Card(props) {
+  const mensagem = "Olá tudo bem? Estou interessado no produto " + props.name
   return (
     <div class="relative flex flex-col bg-white shadow-md w-auto rounded-2xl my-2 cursor-pointer">
       <div class="overflow-x-hidden rounded-2xl relative">
@@ -18,10 +20,10 @@ function Card(props) {
         </div>
       </div>     
       <div className="flex items-end flex-grow">
-      <div class="mb-3 mx-2 text-center bg-[#40b355] w-full rounded-xl cursor-pointer p-2.5 bg-gray">
+      <a href={"https://wa.me/553799414303?text=" + encodeURI(mensagem)} rel="noreferrer" target="_blank" class="mb-3 mx-2 text-center bg-[#40b355] w-full rounded-xl cursor-pointer p-2.5 bg-gray">
         <span className="text-white text-sm">SABER MAIS</span>
         <i id="whats-icon" class="ml-2 fa-brands fa-whatsapp fa-xl"></i>
-      </div>
+      </a>
       </div> 
     </div>
   );
